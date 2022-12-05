0b297f8be33e48e0a0017a57092f79b3a94f1e20
zakinadhif <zaki.nadhif45@gmail.com>
2022-12-06 06:11:44 +0700

    Write Note table migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Note } from "./entity/Note"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Note } from "./entity/Note";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "",
-    database: "alpinemd_ts",
-    synchronize: true,
-    logging: false,
-    entities: [Note],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "",
+  database: "alpinemd_ts",
+  synchronize: true,
+  logging: false,
+  entities: [Note],
+  migrations: ["src/migration/*.ts"],
+  subscribers: [],
+});
